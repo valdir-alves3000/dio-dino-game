ff3a26481fbf4bce8eeac1caa44123967f9f72ed
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2022-03-28 08:53:00 -0300

    Create Dino Game

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dino Game
+
+![plot](https://raw.githubusercontent.com/valdir-alves3000/dio-shopping/main/public/images/desafio-dio.png)
+
+### Preview
+
+Print do projeto finalizado..
+
+![plot](./assets/snapshot.png)
+
+## Projeto
+
+Este é um projeto desenvolvido como forma de desafio para dissiminação de conhecimento dentro do Bootcamp [Órbi Web Games Developer](https://dio.me/bootcamp/orbi-web-game-developer?ref=WXSFIF1TPZ) da plataforma DIO, venha fazer parte deste time! :wink:
---
+++ js/script.js
@@ -0,0 +1,83 @@
+const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+const score = document.querySelector(".score");
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+let scoreValue = 0;
+
+const handleKeyUp = (event) => {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jumping();
+    }
+  }
+};
+
+const jumping = () => {
+  isJumping = true;
+  scoreValue += 10;
+  score.innerHTML = "Score: " + scoreValue;
+
+  dino.style.transform = "rotate(180deg)";
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      dino.style.transform = "rotate(360deg)";
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + "px";
+        }
+      }, 20);
+    } else {
+      position += 20;
+      dino.style.bottom = position + "px";
+    }
+  }, 20);
+};
+
+const createCactus = () => {
+  const cactus = document.createElement("div");
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
+
+  cactus.classList.add("cactus");
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + "px";
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftTimer);
+      isGameOver = true;
+
+      document.body.innerHTML = `
+      <h1 class="game-over">Fim de jogo</h1>   
+      <p>Precione F5 para reiniciar<p>   
+      <div class="background">
+      <div class="score">Score: ${scoreValue}</div>      
+      <div class="dino-over"></div>
+      </div>      
+      `;
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + "px";
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+};
+
+createCactus();
+
+document.addEventListener("keyup", handleKeyUp);
